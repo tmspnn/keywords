e865e22db3b45c94a21f01d82f041ee6b05592ad
Thomas Peng <tmspnn@gmail.com>
2021-06-12 13:41:19 +0800

    Refactor.

--- src/resources/TokResource.py
+++ src/resources/TokResource.py
@@ -4,15 +4,23 @@ from hanlp.utils.rules import split_sentence
 class TokResource:
     def __init__(self, HanLP) -> None:
         self.HanLP = HanLP
+        self.long_sentence_words = 20
 
     def on_post(self, req, res):
         text = req.media.get("text")
-        result = {}
+        sentences = []
 
         if isinstance(text, str):
-            sentences = split_sentence(text)
-            result = self.HanLP(sentences, tasks="tok")
+            self.add_to_sentences(sentences, text)
         elif isinstance(text, list):
-            result = self.HanLP(text, tasks="tok")
+            for item in text:
+                self.add_to_sentences(sentences, item)
 
-        res.media = result
+        res.media = self.HanLP(sentences, tasks="tok")
+
+    def add_to_sentences(self, sentences, text):
+        if len(text) < self.long_sentence_words:
+            sentences.append(text)
+        else:
+            for s in split_sentence(text):
+                sentences.append(s)
